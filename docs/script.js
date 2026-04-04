c9bcc3930901e9adb7b5102fd9a32b41bd88f162
ph <paulo170306@gmail.com>
2026-04-04 19:34:04 -0300

    atualização de layout

--- docs/script.js
+++ docs/script.js
@@ -1,9 +1,17 @@
-const word = "jason";
+let minT = 1;
+const maxT = 6;
 
-//passa pra proxima caixa e verifica se é letra ou não
+const word = "jason";
+let lastId = 15;
+
 function nxL(event, element) {
+  if (element.readOnly) {
+    event.preventDefault();
+    return;
+  }
+
   let key = event.key;
-  let id = parseInt(element.id);
+  let id = parseInt(element.id[1]);
   let newId;
 
   if (key === "Backspace") {
@@ -12,7 +20,8 @@ function nxL(event, element) {
       if (newId <= 0) {
         newId = 5;
       }
-      document.getElementById(newId.toString()).focus();
+      let tId = minT.toString() + newId.toString();
+      document.getElementById(tId).focus();
     } else {
       element.value = "";
     }
@@ -22,23 +31,27 @@ function nxL(event, element) {
     if (newId <= 0) {
       newId = 5;
     }
-    document.getElementById(newId.toString()).focus();
+    let tId = minT.toString() + newId.toString();
+    document.getElementById(tId).focus();
     event.preventDefault();
   } else if (key === "ArrowRight") {
     newId = (id % 5) + 1;
-    document.getElementById(newId.toString()).focus();
+    let tId = minT.toString() + newId.toString();
+    document.getElementById(tId).focus();
     event.preventDefault();
   } else if (key === "Enter") {
     verify();
     event.preventDefault();
   } else if (key == " ") {
     newId = (id % 5) + 1;
-    document.getElementById(newId.toString()).focus();
+    let tId = minT.toString() + newId.toString();
+    document.getElementById(tId).focus();
     event.preventDefault();
   } else if (/[A-Za-z]/.test(key) && key.length === 1) {
     element.value = key;
     newId = (id % 5) + 1;
-    document.getElementById(newId.toString()).focus();
+    let tId = minT.toString() + newId.toString();
+    document.getElementById(tId).focus();
     event.preventDefault();
   } else {
     event.preventDefault();
@@ -47,38 +60,71 @@ function nxL(event, element) {
 
 //verifica os acertos
 function verify() {
-    for(let i = 1; i <= 5; i++){
-        let elem = document.getElementById(i.toString());
-        if(elem.value == ''){
-            return;
-        }
+  for (let i = 1; i <= 5; i++) {
+    let tId = minT.toString() + i.toString();
+    let elem = document.getElementById(tId);
+    if (elem.value == "") {
+      return;
     }
+  }
 
-    let win = 0;
+  let win = 0;
 
-    for(let i = 1; i <= 5; i++){
-        let elem = document.getElementById(i.toString());
-        elem.ariaReadOnly = true
-        let input = elem.value.toLowerCase();
-        let inputR = word[i-1].toLowerCase(); 
+  for (let i = 1; i <= 5; i++) {
+    let tId = minT.toString() + i.toString();
+    let elem = document.getElementById(tId);
+    elem.readOnly = true;
 
-        if(word.includes(input)){
-            if(inputR == input){
-                elem.style.backgroundColor = 'Green';
-                win++;
-            }else{
-                elem.style.backgroundColor = 'Yellow';
-            }
-        }else{
-            elem.style.backgroundColor = 'Red';
-        }
+    let input = elem.value.toLowerCase();
+    let inputR = word[i - 1].toLowerCase();
+
+    if (word.includes(input)) {
+      if (inputR == input) {
+        elem.style.backgroundColor = "Green";
+        win++;
+      } else {
+        elem.style.backgroundColor = "Yellow";
+      }
+    } else {
+      elem.style.backgroundColor = "Red";
     }
+  }
 
-    if(win == 5){
-        showWin();
-    }
+  if (win == 5) {
+    showWin();
+  } else if(minT < maxT){
+    minT++;
+    newRow();
+  } else {
+    showLose();
+  }
+}
+
+function newRow() {
+  let parent = document.getElementById("gameArea");
+  for(let i = 1; i <= 5; i++){
+    let child = document.createElement("input");
+    let tId = minT.toString() + i.toString();
+    addAtributes(child, tId);
+
+    parent.appendChild(child);
+  }
+  lastId += 10;
+  document.getElementById(minT.toString() + "1").focus();
+}
+
+function addAtributes(child, i) {
+    child.setAttribute('type', 'text');
+    child.setAttribute('id', i);
+    child.setAttribute('name', 'L');
+    child.setAttribute('maxLength', '1');
+    child.setAttribute('onkeyDown', 'nxL(event, this)');
+}
+
+function showLose() {
+  document.getElementById("lose").style.display = "block";
 }
 
 function showWin() {
-    document.getElementById('win').style.display = 'block';
+  document.getElementById("win").style.display = "block";
 }
